6344f0ac582b29c5339a68fbe8f03d7ea9a48eea
futurGH <me@futuristick.ca>
2024-05-10 14:25:19 -0400

    tracking

--- app/root.tsx
+++ app/root.tsx
@@ -133,6 +133,14 @@ export default function App() {
 				<meta property="twitter:card" content="summary_large_image" />
 				<Meta />
 				<Links />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+				!function(w,d){ if(!w.rdt){ var p=w.rdt=function(){ p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments) };p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s) } }(window,document);rdt('init','a2_eyslrdeybm4t');rdt('track',
+				'PageVisit');
+			`,
+					}}
+				/>
 			</head>
 			<body className="antialiased scroll-smooth font-figtree selection:bg-accent selection:text-yogurt-100">
 				<CartProvider>
