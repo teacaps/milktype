f762d43ee8c40cc879f6ec008630af6fe90a68eb
futurGH <me@futuristick.ca>
2024-11-12 21:43:11 -0500

    Fix poster's keycaps title margin

--- app/routes/products.posters-keycaps.tsx
+++ app/routes/products.posters-keycaps.tsx
@@ -76,11 +76,11 @@ export default function PostersKeycaps() {
 	return (
 		<Layout>
 			<Container className="py-8 sm:py-16 sm:w-full">
-				<main className="flex flex-col md:flex-row w-full gap-10">
+				<main className="flex flex-col md:flex-row w-full">
 					<div className="md:basis-1/2 lg:basis-2/5 w-full max-h-[40vh] sm:max-h-[50vh] self-center md:self-start aspect-square">
 						<ProductImageGrid images={GRID_IMAGES} />
 					</div>
-					<section className="md:basis-1/2 lg:basis-3/5 flex flex-col gap-y-12">
+					<section className="md:basis-1/2 lg:basis-3/5 flex flex-col gap-y-12 mt-10 md:mt-0 md:ml-10">
 						<div className="flex flex-col gap-y-6">
 							<h1 className="text-4xl font-medium text-cocoa-120">{"poster's keycaps"}</h1>
 							<p
