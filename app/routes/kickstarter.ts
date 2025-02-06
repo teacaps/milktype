1c49e44ee6da43be4105dd4afaef56496dc23169
futurGH <me@futuristick.ca>
2025-02-06 13:21:45 -0500

    Add Discord redirect, remove Kickstarter redirect

--- app/routes/kickstarter.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from "@remix-run/server-runtime";
-
-export function loader() {
-	return redirect("https://kickstarter.com/projects/milktype/sprout-75-the-bubble-tea-mechanical-keyboard", 301);
-}
