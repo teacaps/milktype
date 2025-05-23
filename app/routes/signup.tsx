7bea184dc1bd69cd86f527e5734ef699fbf3099d
futurGH <me@futuristick.ca>
2025-05-22 23:03:13 -0400

    Handle all errors on signup

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -4,41 +4,46 @@ import { shopifyApi, LATEST_API_VERSION } from "@shopify/shopify-api";
 import "@shopify/shopify-api/adapters/web-api";
 
 export async function action({ request, context }: ActionFunctionArgs) {
-	const fd = await request.formData();
-	const email = fd.get("email")?.toString();
+	try {
+		const fd = await request.formData();
+		const email = fd.get("email")?.toString();
 
-	if (!email) {
-		return json({ error: "email is required" }, { status: 200 });
+		if (!email) {
+			return json({ error: "email is required" }, { status: 200 });
+		}
+
+		const hostname = new URL(request.url).hostname;
+
+		const shopify = shopifyApi({
+			adminApiAccessToken: context.env.PRIVATE_ADMIN_API_TOKEN,
+			apiKey: context.env.PRIVATE_ADMIN_API_KEY,
+			apiSecretKey: context.env.PRIVATE_ADMIN_API_SECRET_KEY,
+			scopes: ["read_customers", "write_customers"],
+			hostName: hostname,
+			hostScheme: hostname.startsWith("localhost") ? "http" : "https",
+			apiVersion: LATEST_API_VERSION,
+			isEmbeddedApp: false,
+			isCustomStoreApp: true,
+		});
+
+		const gqlClient = new shopify.clients.Graphql({
+			session: shopify.session.customAppSession(context.env.PUBLIC_STORE_DOMAIN),
+		});
+
+		const { data, errors } = await gqlClient.request(CREATE_CUSTOMER_MUTATION, {
+			variables: { email },
+		});
+
+		if (errors) {
+			console.error(errors.message, JSON.stringify(errors.graphQLErrors, null, 4));
+			return json({ error: errors.message }, { status: 200 });
+		}
+
+		return json({ response: data }, { status: 200 });
+	} catch (error) {
+		console.error("Unhandled error at signup:", error);
+		return json({ error: "an unknown error occurred" }, { status: 200 });
 	}
-
-	const hostname = new URL(request.url).hostname;
-
-	const shopify = shopifyApi({
-		adminApiAccessToken: context.env.PRIVATE_ADMIN_API_TOKEN,
-		apiKey: context.env.PRIVATE_ADMIN_API_KEY,
-		apiSecretKey: context.env.PRIVATE_ADMIN_API_SECRET_KEY,
-		scopes: ["read_customers", "write_customers"],
-		hostName: hostname,
-		hostScheme: hostname.startsWith("localhost") ? "http" : "https",
-		apiVersion: LATEST_API_VERSION,
-		isEmbeddedApp: false,
-		isCustomStoreApp: true,
-	});
-
-	const gqlClient = new shopify.clients.Graphql({
-		session: shopify.session.customAppSession(context.env.PUBLIC_STORE_DOMAIN),
-	});
-
-	const { data, errors } = await gqlClient.request(CREATE_CUSTOMER_MUTATION, {
-		variables: { email },
-	});
-
-	if (errors) {
-		console.error(errors.message, JSON.stringify(errors.graphQLErrors, null, 4));
-		return json({ error: errors.message }, { status: 200 });
-	}
-
-	return json({ response: data }, { status: 200 });
 }
 
 const CREATE_CUSTOMER_MUTATION = `#graphql
