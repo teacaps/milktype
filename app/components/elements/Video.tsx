3cf10ed7190affb2952d7202878f768f92f09837
futurGH <me@futuristick.ca>
2025-07-19 22:14:17 -0400

    Fix video poster URL construction

--- app/components/elements/Video.tsx
+++ app/components/elements/Video.tsx
@@ -6,12 +6,13 @@ export interface VideoProps extends VideoHTMLAttributes<HTMLVideoElement> {
 	alt: string;
 }
 
-export const Video = forwardRef<HTMLVideoElement, VideoProps>(({ src, alt, className, ...props }, ref) => {
+export const Video = forwardRef<HTMLVideoElement, VideoProps>(({ src, alt, poster, className, ...props }, ref) => {
 	return (
 		<>
 			<video
-				src={`https://img.milktype.co/${src}`}
+				src={src.startsWith("http") ? src : `https://img.milktype.co/${src}`}
 				aria-label={alt}
+				poster={!poster || poster?.startsWith("http") ? poster : `https://img.milktype.co/${poster}`}
 				className={twMerge("object-cover object-center", className)}
 				ref={ref}
 				{...props}
