b141f01b63109154b34ce0d39c73d48f21680f23
futurGH <me@futuristick.ca>
2025-08-06 22:16:20 -0400

    Add banner

--- app/components/global/Navigation.tsx
+++ app/components/global/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Await, Link, NavLink, useRouteLoaderData } from "react-router";
+import { Await, Link, NavLink, useLocation, useRouteLoaderData } from "react-router";
 import { Wordmark } from "~/assets/Wordmark";
 import { CartIcon } from "~/assets/icons/Cart";
 import { Container } from "~/components/global/Container";
@@ -9,46 +9,55 @@ import type { RootLoader } from "~/root";
 
 export function Navigation() {
 	const { cartVisible, setCartVisible } = useCartVisibility();
+	const { pathname } = useLocation();
 	return (
-		<Container
-			as="header"
-			className="flex relative w-full flex-row py-8 items-center z-10"
-			style={{ viewTransitionName: "navigation" }}>
-			<Link
-				to="/"
-				className="mr-auto text-cocoa-120 hover:text-accent focus-visible:text-accent focus-visible:outline-none"
-				aria-label="Home"
-				viewTransition>
-				<Wordmark className="h-8 xs:h-10" />
-			</Link>
-			<nav className="flex flex-row flex-grow sm:flex-grow-0 items-center justify-between sm:justify-start sm:gap-x-24 pl-8 sm:pl-0 font-medium text-cocoa-120 text-base leading-6 xs:text-lg">
-				<NavLink
-					prefetch="intent"
-					to="/sprout75"
-					className={({ isActive }) =>
-						twJoin(
-							isActive && "underline",
-							"hover:text-accent focus-visible:text-accent rounded-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-4 focus-visible:ring-accent active:font-medium",
-						)
-					}
+		<Container as="header" className="flex flex-col w-full z-10">
+			{pathname !== "/sprout75" ? (
+				<a
+					href="/sprout75"
+					className="w-full py-2 px-4 mt-4 text-xs md:text-sm text-center text-balance font-medium text-yogurt-100 bg-accent rounded-full hover:bg-lilac">
+					studio cleaning sale — save 15% now! →
+				</a>
+			) : null}
+			<div
+				className="flex flex-row w-full relative items-center py-8"
+				style={{ viewTransitionName: "navigation" }}>
+				<Link
+					to="/"
+					className="mr-auto text-cocoa-120 hover:text-accent focus-visible:text-accent focus-visible:outline-none"
+					aria-label="Home"
 					viewTransition>
-					sprout 75
-				</NavLink>
-				<button
-					type="button"
-					className="relative flex group rounded-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-8 focus-visible:ring-accent"
-					onClick={() => setCartVisible((show) => !show)}>
-					<CartIcon
-						className={twJoin(
-							"h-4 xs:h-5 sm:h-6",
-							cartVisible ? "fill-accent" : "fill-cocoa-120",
-							"group-hover:fill-accent group-focus-visible:fill-accent",
-						)}
-					/>
-					<CartBadge />
-				</button>
-			</nav>
-			<Cart />
+					<Wordmark className="h-8 xs:h-10" />
+				</Link>
+				<nav className="flex flex-row flex-grow sm:flex-grow-0 items-center justify-between sm:justify-start sm:gap-x-24 pl-8 sm:pl-0 font-medium text-cocoa-120 text-base leading-6 xs:text-lg">
+					<NavLink
+						prefetch="intent"
+						to="/sprout75"
+						className={({ isActive }) =>
+							twJoin(
+								isActive && "underline",
+								"hover:text-accent focus-visible:text-accent rounded-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-4 focus-visible:ring-accent active:font-medium",
+							)
+						}
+						viewTransition>
+						sprout 75
+					</NavLink>
+					<button
+						type="button"
+						className="relative flex group rounded-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-8 focus-visible:ring-accent"
+						onClick={() => setCartVisible((show) => !show)}>
+						<CartIcon
+							className={twJoin(
+								"h-4 xs:h-5 sm:h-6",
+								cartVisible ? "fill-accent" : "fill-cocoa-120",
+								"group-hover:fill-accent group-focus-visible:fill-accent",
+							)}
+						/>
+						<CartBadge />
+					</button>
+				</nav>
+				<Cart />
+			</div>
 		</Container>
 	);
 }
